38ab7e98967917a2dd7a4451413deb57d35b5b2b
Sean Henry Lewis <96705270+seanhlewis@users.noreply.github.com>
2022-06-12 22:55:18 -0700

    Update index.php

--- index.php
+++ index.php
@@ -1,1 +1,10 @@
+require_once 'Mail.php';
 
+$wgSMTP = array(
+    'host' => 'tls://smtp.sendgrid.net',
+    'IDHost' => 'heroku.com',
+    'port' => 587,
+    'username' => getenv("SENDGRID_USERNAME"), 
+    'password' => getenv("SENDGRID_PASSWORD"),
+    'auth' => true
+ );
